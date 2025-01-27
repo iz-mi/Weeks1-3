9714ebe1c24500cfee5c9351fa573c0e026ec016
iz-mi <wangmic@sheridancollege.ca>
2025-01-27 00:01:29 -0500

    speedometer graphics added, gaspedal and brakepedal scripts split

--- Assets/MechanicalDrawing/Scripts/brakePedal.cs
+++ Assets/MechanicalDrawing/Scripts/brakePedal.cs
@@ -2,29 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gasPedal : MonoBehaviour
+public class brakePedal : MonoBehaviour
 {
     //create the property of 'wheel' so that rotation can be modified
     public wheelController backWheel;
     public wheelController frontWheel;
 
     //create an animation curve that affects the gas Pedal control
-    public AnimationCurve gasPedalScale;
+    public AnimationCurve brakePedalScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //click the G key to increase car speed (wheelRot)
-        if (Input.GetKeyDown(KeyCode.G))
+        //click the B key to decrease car speed (wheelRot)
+        if (Input.GetKeyDown(KeyCode.B))
         {
-            backWheel.wheelRot += 1.0f;
-            frontWheel.wheelRot += 1.0f;
+            backWheel.wheelRot -= 1.0f;
+            frontWheel.wheelRot -= 1.0f;
         }
     }
 }
